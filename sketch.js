234c1f44358c1b38fb1b36913a3225fc29e8c67b
shirishagajam <64522203+shirishagajam@users.noreply.github.com>
2020-10-10 21:12:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
-
 var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5;
 
 var roofObject;
 
 var rope1, rope2, rope3, rope4, rope5;
+var bobDiameter;
 
 function preload()
 {
@@ -30,15 +30,21 @@ function setup() {
 	bobObject5 = new Bob(460, 350, 40);
 
 	roofObject = new Roof(381,120,237,20);
+	bobDiameter = 40;
+	
+	rope1=new Rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0);
+	
 
-	rope1 = new Rope(bobObject1.body, roofObject.body);
-	rope2 = new Rope(bobObject2.body, roofObject.body);
-	rope3 = new Rope(bobObject3.body, roofObject.body);
-	rope4 = new Rope(bobObject4.body, roofObject.body);
-	rope5 = new Rope(bobObject5.body, roofObject.body);
+	
+	rope1=new Rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
+
+	rope2=new Rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
+	rope3=new Rope(bobObject3.body,roofObject.body,0, 0)
+	rope4=new Rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
+	rope5=new Rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
+
 
 	Engine.run(engine);
-  
 }
 
 
