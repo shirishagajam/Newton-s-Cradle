0348d36c2b2696e7698ae664ef1a61789c4ebcb8
shirishagajam <64522203+shirishagajam@users.noreply.github.com>
2020-10-10 21:13:07 +0530

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -1,31 +1,32 @@
-class Rope {
-    constructor(body1, body2, offsetX, offsetY){
+class Rope{
+	constructor(body1,body2, offsetX, offsetY)
+	{
+		this.offsetX=offsetX;
+		this.offsetY=offsetY;
+		var rope_options={
+			bodyA:body1,
+			bodyB:body2,
+			pointB:{x:this.offsetX, y:this.offsetY}
+		}
+		//console.log(options);
+		this.rope=Constraint.create(rope_options)
+		World.add(world,this.rope)
+	}
 
-        this.offsetX = offsetX;
-        this.offsetX = offsetX;
+	display()
+	{
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
 
-        var rope_options = {
-            bodyA:body1,
-            bodyB:body2,
-            pointB:{x:this.offsetX, y:this.offsetY}
-        }
-        
-        this.rope = Constraint.create(rope_options);
-        World.add(world,this.rope);
-    }
-    display(){
-        var pointA = this.rope.bodyA.position;
-        var pointB = this.rope.bodyB.position;
+		strokeWeight(2);
 
-        strokeWeight(2);
+		var Anchor1X=pointA.x
+		var Anchor1Y=pointA.y
 
-        var Anchor1X = pointA.x
-        var Anchor1Y = pointA.y
+		var Anchor2X=pointB.x+this.offsetX
+		var Anchor2Y=pointB.y+this.offsetY
 
-        var Anchor2X = pointB.x + this.offsetX
-        var Anchor2Y = pointB.y + this.offsetY
+		line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
+	}
 
-
-        line(Anchor1X, Anchor1Y, Anchor2X, Anchor2Y);
-    }
 }
